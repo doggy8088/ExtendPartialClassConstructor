cda9aa6f0ad69900dbee94f62ba6e272eed0c3ff
黃保翕Will <will.huang@miniasp.com>
2013-06-18 19:47:30 +0800

    簽入範例專案 (含 Partial Class / Buddy Class )
    使用 Visual Studio 2012 Update 2
    使用 Entity Framework 5.0
    使用 ASP.NET MVC 4.0
    使用 .NET Framework 4.0
    包含一個範例資料庫 (在 App_Data 目錄下)
    已建立 Entity Framework 實體資料模型 (EDMX)
    已建立實體資料模型所需的 Partial Class / Buddy Class

---
+++ MvcApplication1/MvcApplication1/Models/Partials/Product.Partial.cs
@@ -0,0 +1,25 @@
+namespace MvcApplication1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        public int ProductId { get; set; }
+        
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        public string ProductName { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,18 @@ ExtendPartialClassConstructor
 =============================
 
 此範例專案用來示範如何擴充部分類別中的 建構子 (Constructor)
+
+建置要求
+=======
+
+* 使用 Visual Studio 2012 Update 2
+* * NuGet 請升級到最新版
+
+專案環境
+=======
+* 使用 Entity Framework 5.0
+* 使用 ASP.NET MVC 4.0
+* 使用 .NET Framework 4.0
+* 包含一個範例資料庫 (在 App_Data 目錄下)
+* 已建立 Entity Framework 實體資料模型 (EDMX)
+* 已建立實體資料模型所需的 Partial Class / Buddy Class 
